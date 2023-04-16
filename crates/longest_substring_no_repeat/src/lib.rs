5450408eaef0ddf02359d0301ecc8d26085b284a
c6supper@hotmail.com <c6supper@hotmail.com>
2023-04-16 21:56:21 +0800

    update

---
+++ crates/longest_substring_no_repeat/src/lib.rs
@@ -0,0 +1,64 @@
+//* 3. Longest Substring Without Repeating Characters
+//* Medium
+//* 
+//* Given a string s, find the length of the longest substring without repeating characters.
+//* 
+//*  
+//* 
+//* Example 1:
+//* 
+//* Input: s = "abcabcbb"
+//* Output: 3
+//* Explanation: The answer is "abc", with the length of 3.
+//* Example 2:
+//* 
+//* Input: s = "bbbbb"
+//* Output: 1
+//* Explanation: The answer is "b", with the length of 1.
+//* Example 3:
+//* 
+//* Input: s = "pwwkew"
+//* Output: 3
+//* Explanation: The answer is "wke", with the length of 3.
+//* Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+//*  
+//* 
+//* Constraints:
+//* 
+//* 0 <= s.length <= 5 * 104
+//* s consists of English letters, digits, symbols and spaces.
+
+pub struct Solution {}
+
+impl Solution {
+    pub fn length_of_longest_substring(s: String) -> i32 {
+        let (mut left,mut right) = (0,0);
+        let mut sub_map = std::collections::BTreeMap::new();
+        let mut max_lens = 0;
+
+        for (i,c) in s.chars().enumerate() {
+            if sub_map.contains_key(&c) {
+                while sub_map.pop_first(&c);
+            }
+            else {
+                sub_map.insert(c, i);
+                right = i;
+                if right - left > max_lens {
+                    max_lens = right - left;
+                }
+            }
+        }
+
+        max_lens
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+//     #[test]
+//     fn test_1() {
+//         assert_eq!(5, Solution::minimize_array_value(vec![3, 7, 1, 6]));
+//     }
+// }
