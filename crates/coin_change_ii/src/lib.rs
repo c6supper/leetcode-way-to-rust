958d0145283f5ed4347ef832630ebb5ae19c4f0d
c6supper@hotmail.com <c6supper@hotmail.com>
2023-06-20 02:09:51 +0000

    update

--- crates/coin_change_ii/src/lib.rs
+++ crates/coin_change_ii/src/lib.rs
@@ -38,7 +38,17 @@
 pub struct Solution {}
 
 impl Solution {
-    pub fn change(amount: i32, coins: Vec<i32>) -> i32 {}
+    pub fn change(amount: i32, coins: Vec<i32>) -> i32 {
+        let mut dp = vec![0; amount as usize + 1];
+        dp[0] = 1;
+
+        for i in 0..coins.len() {
+            for j in coins[i] as usize..dp.len() {
+                dp[j] += dp[j - coins[i] as usize];
+            }
+        }
+        dp[amount as usize]
+    }
 }
 
 #[cfg(test)]
