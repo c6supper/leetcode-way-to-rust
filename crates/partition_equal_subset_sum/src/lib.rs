ef31073a01521b52525b2116d0b2796e51b4f84a
c6supper@hotmail.com <c6supper@hotmail.com>
2023-07-03 23:22:12 +0800

    update

--- crates/partition_equal_subset_sum/src/lib.rs
+++ crates/partition_equal_subset_sum/src/lib.rs
@@ -1,31 +1,31 @@
-//* 416. Partition Equal Subset Sum
-//* Medium
-//*
-//* Given an integer array nums, return true if you can partition the array into two subsets such that the sum of the elements in both subsets is equal or false otherwise.
-//*
-//*
-//*
-//* Example 1:
-//*
-//* Input: nums = [1,5,11,5]
-//* Output: true
-//* Explanation: The array can be partitioned as [1, 5, 5] and [11].
-//* Example 2:
-//*
-//* Input: nums = [1,2,3,5]
-//* Output: false
-//* Explanation: The array cannot be partitioned into equal sum subsets.
-//*
-//*
-//* Constraints:
-//* 1 <= nums.length <= 200
-//* 1 <= nums[i] <= 100
+//! 416. Partition Equal Subset Sum
+//! Medium
+//!
+//! Given an integer array nums, return true if you can partition the array into two subsets such that the sum of the elements in both subsets is equal or false otherwise.
+//!
+//!
+//!
+//! Example 1:
+//!
+//! Input: nums = [1,5,11,5]
+//! Output: true
+//! Explanation: The array can be partitioned as [1, 5, 5] and [11].
+//! Example 2:
+//!
+//! Input: nums = [1,2,3,5]
+//! Output: false
+//! Explanation: The array cannot be partitioned into equal sum subsets.
+//!
+//!
+//! Constraints:
+//! 1 <= nums.length <= 200
+//! 1 <= nums[i] <= 100
 
 pub struct Solution {}
 
 impl Solution {
     pub fn can_partition(nums: Vec<i32>) -> bool {
-        let half:usize = nums.iter().sum::<i32>() as usize / 2;
+        let half: usize = nums.iter().sum::<i32>() as usize / 2;
 
         if 2 * half < nums.iter().sum::<i32>() as usize {
             return false;
@@ -36,7 +36,8 @@ impl Solution {
         for i in 0..nums.len() {
             let mut j = half;
             while j >= nums[i] as usize {
-                dp[j] = std::cmp::max(dp[j], dp[j - nums[i] as usize] + nums[i]);
+                dp[j] =
+                    std::cmp::max(dp[j], dp[j - nums[i] as usize] + nums[i]);
                 j -= 1
             }
         }
