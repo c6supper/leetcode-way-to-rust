726123bf5f5142202eda2460c8f3ff1d82af7898
c6supper@hotmail.com <c6supper@hotmail.com>
2023-07-15 13:35:35 +0800

    update

--- crates/house_robber/src/lib.rs
+++ crates/house_robber/src/lib.rs
@@ -28,7 +28,16 @@
 pub struct Solution {}
 
 impl Solution {
-    pub fn rob(nums: Vec<i32>) -> i32 {}
+    pub fn rob(nums: Vec<i32>) -> i32 {
+        let mut dp = vec![0; nums.len() + 1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+
+        for i in 2..dp.len() {
+            dp[i] = std::cmp::max(dp[i - 2] + nums[i - 1], dp[i - 1]);
+        }
+        dp[nums.len()]
+    }
 }
 
 #[cfg(test)]
