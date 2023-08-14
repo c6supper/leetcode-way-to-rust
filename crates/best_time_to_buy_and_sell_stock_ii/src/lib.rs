a2fe57519fccc0753954ef9ff626c740b410e017
c6supper@hotmail.com <c6supper@hotmail.com>
2023-08-14 22:49:09 +0800

    update

--- crates/best_time_to_buy_and_sell_stock_ii/src/lib.rs
+++ crates/best_time_to_buy_and_sell_stock_ii/src/lib.rs
@@ -34,7 +34,19 @@
 pub struct Solution {}
 
 impl Solution {
-    pub fn max_profit(prices: Vec<i32>) -> i32 {}
+    pub fn max_profit(prices: Vec<i32>) -> i32 {
+        let mut dp = vec![vec![0; 2]; prices.len()];
+
+        dp[0][0] = -prices[0];
+        dp[0][1] = 0;
+
+        for i in 1..prices.len() {
+            dp[i][0] = std::cmp::max(dp[i - 1][0], dp[i - 1][1] - prices[i]);
+            dp[i][1] = std::cmp::max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
+        }
+
+        dp[prices.len() - 1][1]
+    }
 }
 
 #[cfg(test)]
